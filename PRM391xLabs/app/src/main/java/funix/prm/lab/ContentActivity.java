a3ce3b4efc16a0dec0c2cd998d1a89df19b2883a
huanzb321 <64452223+huanzb321@users.noreply.github.com>
2021-01-15 13:56:08 +0700

    update file PRM391xLab

---
+++ PRM391xLabs/app/src/main/java/funix/prm/lab/ContentActivity.java
@@ -0,0 +1,14 @@
+package funix.prm.lab;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+
+public class ContentActivity extends Activity {
+    @Override
+    protected void onCreate (@Nullable Bundle savedIntenceState) {
+        super.onCreate(savedIntenceState);
+    }
+}
--- PRM391xLabs/app/src/main/java/funix/prm/lab/MainActivity.java
+++ PRM391xLabs/app/src/main/java/funix/prm/lab/MainActivity.java
@@ -1,14 +1,55 @@
 package funix.prm.lab;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, TAG +" LifeCycle: " + "onCreare", Toast.LENGTH_LONG).show();
+
+        Button btnMove = findViewById(R.id.btnMove);
+        btnMove.setOnClickListener((view) -> {
+            Intent intent = new Intent(MainActivity.this, ContentActivity.class);
+            startActivity(intent);
+        });
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, TAG + " LifeCycle: " + "onStart",Toast.LENGTH_LONG).show();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, TAG + " LifeCycle: " + "onResume",Toast.LENGTH_LONG).show();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, TAG + " LifeCycle: " + "onPause",Toast.LENGTH_LONG).show();
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, TAG + " LifeCycle: " + "onStop",Toast.LENGTH_LONG).show();
+    }@Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this, TAG + " LifeCycle: " + "onRestart",Toast.LENGTH_LONG).show();
+    }
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, TAG + " LifeCycle: " + "onDestroy",Toast.LENGTH_LONG).show();
     }
 }
